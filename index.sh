bc2ec4ecc593984addbb874ac14560292e172012
gholk <c34031328@mail.ncku.edu.tw>
2016-05-02 11:34:09 +0800

    [index.sh] dir assign
    can use `index -o ~/Download ~/Picture .` to index many dir.
    if no path is provided, index.sh will index current directory.
    both `dir/` and `dir` is okey.
    
    subtitude -v to less, rather than vi.

--- index.sh
+++ index.sh
@@ -1,8 +1,60 @@
 #!/usr/bin/bash
 
-find -path './.*' -prune -o -print |  sed -r -e 's/"/%22/g' \
-	-e 's/.*(gif|jpg|png)$/<img src="&">/i' \
-	-e 's/^[^<].*[^>]$/<a href="&">&<\/a>/' \
-	-e '1i<link rel="stylesheet" type="text/css" href="http://gholk.github.io/Documents/word.css">' \
-	-e '1i<meta charset="UTF-8">' > index.html
+# deal with short arguments, store in `$args` . 
+
+## option ##
+#
+# g : do not treat `*.gif` as img; gif animated is large. 
+# o : xdg-open index.html after script. 
+# R : random sort the content of html. 
+#
+
+for arg in $@
+do
+	if [[ $arg =~ ^-[^-] ]] 
+	then 
+		args="$args$arg"
+		shift
+	fi
+done
+
+# deside what sufix is images
+imgtype="gif|jpg|png"
+
+if [[ $args =~ g ]]
+then imgtype="jpg|png" 
+fi
+
+if [[ $args =~ R ]]
+then sortarg='-R'
+fi
+
+if [ -z $@ ]
+then dirs="./"
+fi
+
+for dir in $@ $dirs
+do
+
+	find $dir -path '*/.*' -prune -o -print |\
+		sed -r -e 's/^\.\///g' \
+			-e 's/"/%22/g' \
+			-e "s/.*($imgtype)$/<img src=\"&\" title=\"&\">/i" \
+			-e 's/^[^<].*[^>]$/<a href="&">&<\/a>/' |\
+		sort $sortarg |\
+		sed -e '1i<link rel="stylesheet" type="text/css" href="http://gholk.github.io/Documents/word.css">' \
+			-e '1i<meta charset="UTF-8">' >\
+			"${dir}/index.html"
+	
+	if [[ $args =~ v ]]
+	then less "$dir/index.html"
+	fi
+	
+	if [[ $args =~ o ]]
+	then xdg-open "$dir/index.html"
+	fi
+
+done
+
+exit
 
