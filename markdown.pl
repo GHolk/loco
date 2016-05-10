b04310010ae23a56e9d4f923db7eede2c5346b35
gholk <c34031328@mail.ncku.edu.tw>
2016-05-10 14:55:52 +0800

    [add] markdown supcj tanet
    supcj: remove bomb.
    tanet: add some collage.
    markdown: add <//server.name/path/file> kind of link.

--- markdown.pl
+++ markdown.pl
@@ -1442,7 +1442,7 @@ sub _EncodeBackslashEscapes {
 sub _DoAutoLinks {
 	my $text = shift;
 
-	$text =~ s{<([^'"\s]*//[^'">\s]+)>}{<a href="$1">$1</a>}gi;
+	$text =~ s{<([a-z:]*//[^'">\s]+)>}{<a href="$1">$1</a>}gi;
 
 	# Email addresses: <address@domain.foo>
 	$text =~ s{
