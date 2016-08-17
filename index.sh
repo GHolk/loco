f5d3bb34c85be26a028ab682c2037db758a3d0d1
gholk <c34031328@mail.ncku.edu.tw>
2016-08-17 17:38:52 +0800

    [add] index use ./ not exec script
    old use `exec index.sh`, now use `./index.sh`.
    this make 2 layer, if so slow
    consider use `. ./index.sh`.

--- index.sh
+++ index.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/bash
 
-if [ -d "$1" ]
+OPTIND=1
+
+while getopts 'vo' a
+do
+	case $a in
+	( v )
+		v='less index.html' ;;
+	( o )
+		o='xdg-open index.html' ;;
+	esac
+done
+
+
+eval d=\$$OPTIND
+if [ -d "$d" ]
 then
-	cd "$1" || ( echo "can not cd $1 !">&2 ; exit 1 )
+	cd "$d" || ( echo "can not cd $d !">&2 ; exit 1 )
 	shift
 fi
 
@@ -13,15 +27,8 @@ then
 	( echo "cant overwrite index_old.html">&2 ;  exit 10 )
 fi
 
-# 重導向 stdout 到 index.html ，
-# 並保存 stdout 到 &6 。
-# 在 */index.sh 中可以用 >&6 來訪問 stdout ，
-# 預設則輸出到 index.html. 
-exec 6>&1
-exec >./index.html
-
 if [ -s index.sh ] && [ -x index.sh ]
-then exec ./index.sh $@
+then ./index.sh $@ >index.html
 else
 
 # define head of html
@@ -41,15 +48,9 @@ imgtype="gif|jpg|png"
 find . -not -path '*/.*' |\
 	sed -r -e "s/'/%27/g; s/.*($imgtype)$/<img src='&' title='&'>/i; s/^[^<].*[^>]$/<a href='&'>&<\\/a>/" |\
 	#sort $sortarg |\
-	sed -e "1i$head" 
+	sed -e "1i$head" >index.html
 
 fi
 
-if [ -n "$see" ]
-then less "$dir/index.html"
-fi
-
-if [ -n "$open" ]
-then xdg-open "$dir/index.html"
-fi
-
+$v
+$o
