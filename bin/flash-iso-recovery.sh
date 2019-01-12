5389b4421e353098f21273910a0e55fa208a40ee
gholk <85619andy@gmail.com>
2019-01-12 12:27:52 +0800

    flash iso and backup for recovery

---
+++ bin/flash-iso-recovery.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+set -e # flash disk is dangerous, exit if any errror.
+
+environment_or_assign() {
+    # if variable not defined, set value
+    local name value
+    name=$1
+    value="$2"
+    if eval [ -z "\"\$$name\"" ]
+    then
+        eval "$name='$value'"
+    fi
+}
+
+if [ $# -ne 3 ]
+then
+    cat <<USAGE
+usage
+        $0 cdrom.img /dev/sdz backup.img
+
+recover
+        dd if=backup.img of=/dev/sdz
+USAGE
+    exit 22
+fi
+
+environment_or_assign cdrom "$1"
+environment_or_assign disk "$2"
+environment_or_assign backup "$3"
+
+environment_or_assign cdrom_size $(wc -c "$cdrom" | cut -d ' ' -f 1)
+environment_or_assign disk_size $(
+    fdisk -l "$disk" |
+    sed -E '1{s/.* ([[:digit:]]+) bytes.*/\1/; q}'
+)
+
+if [ $cdrom_size -gt $disk_size ]
+then
+   echo "cdrom size $cdrom_size greater than disk size $disk_size" >&2
+   exit 28
+fi
+
+environment_or_assign dd_block_size 5242880 # 5M
+environment_or_assign dd_count $(expr $cdrom_size / $dd_block_size + 1)
+
+echo backup size is $(expr $dd_block_size \* $dd_count)
+echo backup "$disk" to "$backup"
+
+dd if="$disk" of="$backup" bs=$dd_block_size count=$dd_count
+
+echo write "$cdrom" to "$disk"
+dd if="$cdrom" of="$disk" bs=$dd_block_size
+
+echo recover disk by "dd if='$backup' of='$disk' bs=5M"
