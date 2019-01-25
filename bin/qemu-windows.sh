575d9a3cb59b0ae98311e492af23115d3ad81c83
gholk <85619andy@gmail.com>
2019-01-25 20:55:58 +0800

    windows qemu qcow2 image

--- bin/qemu-windows.sh
+++ bin/qemu-windows.sh
@@ -2,9 +2,9 @@
 # run dual boot windows 10 partition by qemu and ovmf,
 # record option in script.
 
-qemu-system-x86_64 -enable-kvm -snapshot \
-                   -m 1G \
-                   -hda /dev/sda \
+qemu-system-x86_64 -enable-kvm \
+                   -m 8G \
+                   -smp 4 \
                    -pflash /usr/share/ovmf/OVMF.fd \
-                   -net none \
+                   -vga qxl \
                    "$@"
