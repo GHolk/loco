518e7bafb0078670cd35dba84e09d2a4312ad4cd
gholk <85619andy@gmail.com>
2019-03-01 12:25:36 +0800

    check more input

--- bin/tmux-give-p.sh
+++ bin/tmux-give-p.sh
@@ -18,6 +18,21 @@ get_pane() {
     tmux save-buffer -b current_pane -
 }
 
+send_money() {
+    local money
+    money=$1
+    if [ -n "$after_tax" ]
+    then
+        send_wait $money TAB Enter
+    else
+        send_wait $money Enter
+    fi
+}
+
+log() {
+    echo "$@" >&2
+}
+
 ensure_give_menu() {
     tmux select-window -t $ptt_pane
     if get_pane | grep -q "(0)Give *給其他人Ptt幣"
@@ -28,7 +43,7 @@ ensure_give_menu() {
         tmux select-window -t $script_pane
         clear
         cat <<GIVE_P_SCREEN
-【Ｐtt量販店】                   批踢踢實業坊                                  
+【Ｐtt 量販店】                   批踢踢實業坊                                  
 
 
                     > (0)Give        給其他人Ptt幣
@@ -36,11 +51,11 @@ ensure_give_menu() {
                       (2)From        暫時修改故鄉
                       (3)OSong       心情點播機
 
-[2/28 星期四 21:20]二二八紀念日   線上97496人, 我是gholk          [呼叫器]打開 
+[2/28 星期四 21:20]二二八紀念日   線上 97496 人, 我是 gholk     [呼叫器]打開 
 
 
 GIVE_P_SCREEN
-        echo please go to this menu before run this script
+        log please go to this menu before run this script
         exit 2
     fi
 }
@@ -48,18 +63,26 @@ GIVE_P_SCREEN
 
 send_wait() {
     tmux send-keys $*
-    sleep 0.3s
+    sleep 0.5s
 }
 
 send_password() {
     tmux paste-buffer -b ptt_password
-    echo send password
-    sleep 0.3s
+    log send password
+    sleep 0.5s
 }
 
 request_password() {
     get_pane | fgrep -q '請輸入您的密碼:'
 }
+
+valid_username() {
+    local username
+    username="$1"
+    echo $username | egrep -q '^[[:alpha:]][[:alpha:][:digit:]]+$'
+    return $?
+}
+
 
 if [ -z "$TMUX" ]
 then
@@ -80,13 +103,16 @@ set_if_empty user_interval 2
 
 ensure_give_menu
 
-set_if_empty money $1
-[ -n "$1" ] && shift
-
-if [ -z "$money" ]
+if [ -z $money ]
 then
-    echo please input money
-    read money
+    if [ $1 -gt 0 ] 2>/dev/null
+    then 
+        money=$1
+        shift
+    else
+        log please input money
+        read money
+    fi
 fi
 
 if ! tmux save-buffer -b ptt_password - >/dev/null
@@ -99,20 +125,31 @@ then
 fi
 
 
+pipe_username_list() {
+    if [ -n "$username_list" ]
+    then
+        for username in $username_list
+        do echo $username
+        done
+    else
+        log please input username list
+        log each row contain one user
+        cat
+    fi
+}
 
 set_if_empty username_list "$*"
-if [ -z "$username_list" ]
-then
-    echo please input username list
-    echo each row contain one user
-    username_list="`cat`"
-fi
-
 
 set -e
 
-for username in $username_list
+pipe_username_list | while read username
 do
+    if ! valid_username $username
+    then
+        log invalid username $username
+        continue
+    fi
+
     sleep $user_interval
     
     tmux select-window -t $ptt_pane
@@ -120,7 +157,7 @@ do
     send_wait 0 Enter
     send_wait -l "$username"
     send_wait Enter
-    send_wait $money Enter
+    send_money $money
 
     if request_password
     then
@@ -133,9 +170,9 @@ do
 
     tmux select-window -t $script_pane
 
-    echo send $username finish
+    log send $username $money p
 done
 
 tmux delete-buffer -b ptt_password
 tmux delete-buffer -b current_pane
-echo finish all user
+log finish all user
