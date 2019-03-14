5595da1cb749084413b277072392840c4216b625
gholk <85619andy@gmail.com>
2019-03-15 00:37:00 +0800

    reboot another entry by time detect

---
+++ bin/grub-reboot-entry.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ -n "$1" ]
+then next_entry=$1
+else 
+    cat <<EXIT
+usage
+    $0 1
+    # set grub boot entry to 1 in this hour
+EXIT
+    exit 22
+fi
+
+
+set_date_hour_entry() {
+    date=$1
+    hour=$2
+    entry=$3
+    grub-editenv - set gholk_reboot_date=$date
+    grub-editenv - set gholk_reboot_hour=$hour
+    grub-editenv - set gholk_reboot_entry=$next_entry
+}
+
+if [ $next_entry = unset ]
+then
+    grub-editenv - unset gholk_reboot_date
+    grub-editenv - unset gholk_reboot_hour
+    grub-editenv - unset gholk_reboot_entry
+else
+    set_date_hour_entry $(date -u +"%e %k") $next_entry
+fi
+
