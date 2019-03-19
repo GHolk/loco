f44fbcb2248429e2c0eaaf06ccf75107d72582d0
gholk <85619andy@gmail.com>
2019-03-19 09:56:36 +0800

    use at unset grub after reboot

--- bin/grub-reboot-entry.sh
+++ bin/grub-reboot-entry.sh
@@ -21,6 +21,13 @@ set_date_hour_entry() {
     grub-editenv - set gholk_reboot_entry=$next_entry
 }
 
+remove_entry_after_reboot() {
+    # at will run expire job after reboot
+    at now + 30 minutes <<AT
+$0 unset
+AT
+}
+
 if [ $next_entry = unset ]
 then
     grub-editenv - unset gholk_reboot_date
@@ -28,5 +35,6 @@ then
     grub-editenv - unset gholk_reboot_entry
 else
     set_date_hour_entry $(date -u +"%e %k") $next_entry
+    remove_entry_after_reboot
 fi
 
