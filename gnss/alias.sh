13e5b47847af8c0fb8b1c9a34660bc038a152ed4
gholk <85619andy@gmail.com>
2019-10-26 19:53:40 +0800

    gdcat from gipsy?

--- gnss/alias.sh
+++ gnss/alias.sh
@@ -5,3 +5,8 @@ _format_meter=%.5f
 alias xyz2llh='cs2cs -f $_format_degree +proj=cart +to +proj=lonlat'
 alias xyz2twd97='cs2cs -f $_format_meter +proj=cart +to +init=epsg:3826'
 alias llh2twd97='proj -f $_format_meter +init=epsg:3826'
+
+gdcat_to_xyz() {
+    awk '/STA.[XYZ]/ { p[substr($2,10,1)] = $4}
+         /STA.Z/ { epoch = $3; print p["X"],p["Y"],p["Z"], epoch }' "$@"
+}
