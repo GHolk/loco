56643e314c323a97317de0d47d4a90c60593d96b
gholk <85619andy@gmail.com>
2019-12-03 12:34:16 +0800

    adjust width to fit notify box
    * adjust width by fmt
    * assign target in makefile

--- bin/xmotd.sh
+++ bin/xmotd.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 # zenity is able to send notification too
 
+message_box_width=50
+run_and_cat_motd() {
+    run-parts --lsbsysinit /etc/update-motd.d
+    cat /etc/motd
+}
+
 notify-send --urgency=critical \
             --expire-time=60000 \
             motd \
-            "$(run-parts --lsbsysinit /etc/update-motd.d; cat /etc/motd)"
+            "$(run_and_cat_motd | fmt -w $message_box_width)"
