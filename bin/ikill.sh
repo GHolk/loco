c8564cd32133f26778bbb6590cc108bdc073cbf8
gholk <85619andy@gmail.com>
2020-01-05 16:35:19 +0800

    ikill for pkill interactive

---
+++ bin/ikill.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+ps_header=--headers
+for pid in `pgrep "$@"`
+do
+    ps -l $ps_header $pid
+    echo "kill? (Y/n/term/kill/stop/...signal)"
+    read input
+    case "$input" in
+        n*|N*) signal=0 ;;
+        y*|Y*) signal=term ;;
+        "") signal=term ;;
+        *) signal="$input" ;;
+    esac
+
+    kill -s $signal $pid
+
+    ps_header=--no-headers
+done
+
