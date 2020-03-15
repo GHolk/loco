7c06f9091df8ae2cc9d216f6b9006a39a6b9fb94
gholk <85619andy@gmail.com>
2020-03-15 14:38:55 +0800

    remove wrong sysctl namespace sys

--- bin/ip-route-forward.sh
+++ bin/ip-route-forward.sh
@@ -9,8 +9,8 @@ ip address add $local_net_ip dev $local_net_device
 ip route add $local_net_range via $local_net_ip
 
 # allow guest access internet
-sysctl sys.net.ipv4.conf.$local_net_device.forwarding=1
-sysctl sys.net.ipv4.conf.$inter_net_device.forwarding=1
+sysctl net.ipv4.conf.$local_net_device.forwarding=1
+sysctl net.ipv4.conf.$inter_net_device.forwarding=1
 iptables -t nat -A POSTROUTING \
          -s $local_net_range \
          \! -d $local_net_range \
