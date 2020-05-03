a37e2e42ca38261e7802394683562aa4d5b57e30
gholk <85619andy@gmail.com>
2020-05-03 17:54:02 +0800

    work memory usage inform script

--- bin/memory-notify.sh
+++ bin/memory-notify.sh
@@ -1,4 +1,43 @@
 #!/bin/sh
+export PATH=$PATH:/sbin
+
+for event in EXIT INT HUP TERM 
+do trap "on_exit $event" $event
+done
+
+on_exit() {
+    trap - EXIT INT HUP TERM
+    echo exit on $1
+    kill `pgrep --parent $$`
+    exit
+}
+
+swapon_fake() {
+local zram=$(cat zram)
+local swap=$(cat swap)
+cat <<SWAP
+NAME       TYPE            SIZE            USED PRIO
+/dev/dm-4  partition 4030722048           $swap   -2
+/dev/zram0 partition  401784832           $zram  100
+/dev/zram1 partition  401784832           $zram  100
+/dev/zram2 partition  401784832           $zram  100
+/dev/zram3 partition  401784832           $zram  100
+SWAP
+}
+
+if [ -n "$DEBUG" ]
+then
+    debug_directory=/tmp/swapon-fake.$$
+    mkdir $debug_directory
+    cd $debug_directory
+    exec 1>log
+    exec 2>&1
+    echo 0 > swap
+    echo 0 > zram
+    swapon() {
+        swapon_fake
+    }
+fi
 
 while sleep 10
 do
@@ -7,6 +46,10 @@ do
 done | awk '
 BEGIN {
     debug = ENVIRON["DEBUG"]
+    icon["zram"] = "dialog-warning"
+    icon["swap"] = "dialog-error"
+    gate["zram"] = 0.7
+    gate["swap"] = 0
 }
 (debug) { print }
 
@@ -18,11 +61,11 @@ BEGIN {
     use[type] += $4
 }
 /swapon end/ {
-    if (use["swap"] > 0) {
-        print_zenity_message("error", "swap", use["swap"], total["swap"])
+    if (use["swap"] > gate["swap"]) {
+        print_zenity_message(icon["swap"], "swap", use["swap"], total["swap"])
     }
-    else if (use["zram"] / total["zram"] > 0.36) {
-        print_zenity_message("warning", "zram", use["zram"], total["zram"])
+    else if (use["zram"] / total["zram"] > gate["zram"]) {
+        print_zenity_message(icon["zram"], "zram", use["zram"], total["zram"])
     }
     if (debug) {
         print "total", total["zram"], total["swap"]
@@ -34,10 +77,12 @@ BEGIN {
 
 function print_zenity_message(icon, type, use, total) {
     title = "memory notify"
-    message = sprintf("%s used %d MB (%f%%)", type, use / 1000000, use / total)
+    message = sprintf("%s used %d MB (%.1f%%)", type,
+                      use / 1000000, use / total * 100)
     command = sprintf("notify-send --expire-time 7000 --icon %s \"%s\" \"%s\"",
         icon, title, message)
     if (debug) print command
     system(command)
 }
-'
+' &
+wait
