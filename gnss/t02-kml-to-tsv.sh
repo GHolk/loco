0b2385e66d62cf1da7b43589774179002e8c703b
gholk <85619andy@gmail.com>
2020-08-13 14:05:11 +0800

    log pdop from kml

--- gnss/t02-kml-to-tsv.sh
+++ gnss/t02-kml-to-tsv.sh
@@ -14,6 +14,7 @@ sed 's/<kml.*>/<kml>/' \
     -m table \
     -v 'tr/td[.="UTC"]/following-sibling::td' -o , \
     -v 'tr/td[.="Used"]/following-sibling::td' -o , \
+    -v 'tr/td[.="PDOP"]/following-sibling::td' -o , \
     -m 'tr[contains(td, "Sigma")]/following-sibling::tr[position() <= 3]' \
         -v 'td[2]' --if 'position() < 3' -o , --break --break \
     --break --nl \
