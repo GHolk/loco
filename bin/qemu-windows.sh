78fd04dc55a0d51d48065df3caf110725ab7f72d
gholk <85619andy@gmail.com>
2021-10-28 19:19:54 +0800

    add many feature for qemu-windows
    * stdio command line
    * sound card

--- bin/qemu-windows.sh
+++ bin/qemu-windows.sh
@@ -2,9 +2,13 @@
 # run dual boot windows 10 partition by qemu and ovmf,
 # record option in script.
 
-qemu-system-x86_64 -enable-kvm -snapshot \
-                   -m 1G \
-                   -hda /dev/sda \
-                   -pflash /usr/share/ovmf/OVMF.fd \
-                   -net none \
+# file=/usr/share/ovmf/OVMF_CODE.fd,readonly=on
+# file=/usr/share/ovmf/OVMF_VARS.fd
+# -net none
+
+qemu-system-x86_64 -enable-kvm \
+                   -m 2G -smp 2 \
+                   -monitor stdio \
+                   -net user -net nic -soundhw all \
+                   -drive if=pflash,file=/usr/share/ovmf/OVMF.fd,format=raw,readonly=on \
                    "$@"
