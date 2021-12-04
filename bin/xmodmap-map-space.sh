1c6355252a55adad7f20d1fbb06558914c4781e6
gholk <85619andy@gmail.com>
2021-12-04 22:44:55 +0800

    script to map some keys to space
    for harry potter magic awakened ball game.
    space is to play note.
    multiple space mean one can play multiple note at same time.

---
+++ bin/xmodmap-map-space.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+xmodmap -e "keycode 53 = space" \
+        -e "keycode 54 = space" \
+        -e "keycode 58 = space" \
+        -e "keycode 59 = space"
+
+echo "key 'x' 'c' 'm' ',' are mapped to space"
+echo "press enter to recover"
+read wait
+
+xmodmap -e "keycode 53 = x" \
+        -e "keycode 54 = c" \
+        -e "keycode 58 = m" \
+        -e "keycode 59 = comma"
